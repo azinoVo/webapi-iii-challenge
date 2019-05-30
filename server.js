8b69ea82815e4a8d0d19aee0f33169c008361e55
azinoVo <azinovo22@gmail.com>
2019-05-30 17:47:31 -0500

    init commit, boilerplate

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = 'express';
-
+const express = require('express');
 const server = express();
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+
 //custom middleware
+  // - `logger()`
+  // - `logger` logs to the console the following information about each request: request method, request url, and a timestamp
+  // - this middleware runs on every request made to the API
 
 function logger(req, res, next) {
-
+  console.log(`${req.method} method made from ${req.url} at ${new Date().toISOString()}`)
 };
 
 module.exports = server;
